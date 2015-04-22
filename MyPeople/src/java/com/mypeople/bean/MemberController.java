5c87d73ccddf2a6ecd70f96ef944993bdb54063d
sajanamaharjan <sajanamaharjan1@gmail.com>
2015-04-22 15:23:01 -0500

    around me

--- MyPeople/src/java/com/mypeople/bean/MemberController.java
+++ MyPeople/src/java/com/mypeople/bean/MemberController.java
@@ -9,6 +9,8 @@ import com.mypeople.entity.MyPeopleMember;
 import com.mypeople.facade.MyPeopleMemberFacade;
 import com.mypeople.facade.MyPeopleMemberFacadeLocal;
 import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -23,8 +25,12 @@ public class MemberController implements Serializable{
    @EJB
     MyPeopleMemberFacadeLocal mpMemberFacade;
     private MyPeopleMember mpMember;
+    private List<MyPeopleMember> mpMemberList;
     
-
+ @PostConstruct
+    public void initMembers() {
+        mpMemberList = mpMemberFacade.findAll();
+    }
     /**
      * Creates a new instance of MemberBean
      */
@@ -43,6 +49,14 @@ public class MemberController implements Serializable{
 
     public void setMpMember(MyPeopleMember mpMember) {
         this.mpMember = mpMember;
+    }
+
+    public List<MyPeopleMember> getMpMemberList() {
+        return mpMemberList;
+    }
+
+    public void setMpMemberList(List<MyPeopleMember> mpMemberList) {
+        this.mpMemberList = mpMemberList;
     }
    
 
