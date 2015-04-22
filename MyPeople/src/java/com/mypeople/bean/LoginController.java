6cbcb22ce562cdcaca6f6f7bd3ac946470d80dd9
Ashok Subedi <asoka009su@gmail.com>
2015-04-22 12:26:32 -0500

    commiting

---
+++ MyPeople/src/java/com/mypeople/bean/LoginController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mypeople.bean;
+
+import com.mypeople.entity.Member;
+import java.io.IOException;
+import javax.inject.Named;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Ashok Subedi
+ */
+@Named(value = "loginController")
+@SessionScoped
+public class LoginController {
+    
+    private Member member;
+
+    /**
+     * Creates a new instance of LoginController
+     */
+    public LoginController() {
+    }
+    
+    public void logout() throws IOException {
+        member = null;
+        
+        HttpServletRequest hsr = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        hsr.getSession().invalidate();
+        
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        ec.invalidateSession();
+        ec.redirect(ec.getRequestContextPath() + "/faces/" + "index.xhtml");
+        
+        
+//        String result="/index?faces-redirect=true";
+     
+//    FacesContext context = FacesContext.getCurrentInstance();
+//    HttpServletRequest request = (HttpServletRequest)context.getExternalContext().getRequest();
+//     
+//    try {
+//      request.logout();
+//    } catch (ServletException e) {
+//      log.log(Level.SEVERE, "Failed to logout user!", e);
+//      result = "/loginError?faces-redirect=true";
+//    }
+//     
+//    return result;
+
+    }
+    
+}
--- MyPeople/src/java/com/mypeople/facade/MemberFacade.java
+++ MyPeople/src/java/com/mypeople/facade/MemberFacade.java
@@ -6,9 +6,13 @@
 package com.mypeople.facade;
 
 import com.mypeople.entity.Member;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -26,6 +30,24 @@ public class MemberFacade extends AbstractFacade<Member> implements MemberFacade
 
     public MemberFacade() {
         super(Member.class);
+    }
+    
+     public Member findByEmail(String email) {
+        try {
+            if (email == null) {
+                email = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
+            }
+
+            String jpql = "SELECT u from Member u where u.email = :email";
+            Query query = getEntityManager().createQuery(jpql, Member.class);
+            query.setParameter("email", email);
+            return (Member) query.getSingleResult();
+
+        } catch (Exception e) {
+
+            Logger.getLogger(MemberFacade.class.getName()).log(Level.SEVERE, null, e);
+            return null;
+        }
     }
     
 }
